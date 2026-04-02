a6ac3b9fdf970f84ddc29edd6fd92d018492380f
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-02 10:18:55 +0530

    Code was updated

---
+++ Array/Problems/PrefixSum.java
@@ -0,0 +1,17 @@
+class PrefixSum{
+    public static void main(String[] args){
+        int[] arr ={2,4,6,8};
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+
+        System.out.println("Prefix Sum is:");
+
+        System.out.print(prefix[0]+" ");
+        for(int i=1; i<arr.length;i++){
+            prefix[i] = prefix[i-1] + arr[i];
+             System.out.print(prefix[i]+" ");
+        }
+       
+    }
+
+}
